5fe2f38838c3df76a2e9c63bde5371fe286b0e5f
Đorđe Kesić <dorde.kesic@squalomail.com>
2021-01-08 10:00:52 +0100

    #PAC4# replacements

--- lib/Ebizmarts/MailChimp/ListsActivity.php
+++ lib/Ebizmarts/MailChimp/ListsActivity.php
@@ -3,7 +3,7 @@
  * mailchimp-lib Magento Component
  *
  * @category  Ebizmarts
- * @package   mailchimp-lib
+ * @package   #PAC4#
  * @author    Ebizmarts Team <info@ebizmarts.com>
  * @copyright Ebizmarts (http://ebizmarts.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
