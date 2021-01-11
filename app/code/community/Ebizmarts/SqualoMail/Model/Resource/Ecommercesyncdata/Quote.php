e25878a2923e154f9ade111a84c84c4f3527bd09
Đorđe Kesić <dorde.kesic@squalomail.com>
2021-01-11 08:09:23 +0100

    reverts #PAC1# replacements

--- app/code/community/Ebizmarts/SqualoMail/Model/Resource/Ecommercesyncdata/Quote.php
+++ app/code/community/Ebizmarts/SqualoMail/Model/Resource/Ecommercesyncdata/Quote.php
@@ -7,7 +7,7 @@
  * #REPO_NAME# Magento Component
  *
  * @category  Ebizmarts
- * @package   #PAC1#
+ * @package   mc-magento
  * @author    Ebizmarts Team <info@ebizmarts.com>
  * @copyright Ebizmarts (http://ebizmarts.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
