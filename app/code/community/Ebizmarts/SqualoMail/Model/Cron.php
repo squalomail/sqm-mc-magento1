ebddbf2d2bbcae15c6ad5984620da3f76a26353c
Đorđe Kesić <dorde.kesic@squalomail.com>
2021-01-11 08:10:40 +0100

    reverts #PAC3#, #PAC4# replacements

--- app/code/community/Ebizmarts/SqualoMail/Model/Cron.php
+++ app/code/community/Ebizmarts/SqualoMail/Model/Cron.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @category Ebizmarts
- * @package #PAC3#
+ * @package mailchimp-lib
  * @author Ebizmarts Team <info@ebizmarts.com>
  * @copyright Ebizmarts (http://ebizmarts.com)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
--- lib/Ebizmarts/SqualoMail/EcommercePromoRules.php
+++ lib/Ebizmarts/SqualoMail/EcommercePromoRules.php
@@ -4,7 +4,7 @@
  * squalomail-lib Magento Component
  *
  * @category  Ebizmarts
- * @package   #PAC4#
+ * @package   mailchimp-lib
  * @author    Ebizmarts Team <info@ebizmarts.com>
  * @copyright Ebizmarts (http://ebizmarts.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
